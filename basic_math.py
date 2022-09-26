7a0692c8c64a993059cb18f2db0cd16410b118a7
ICS3U.1 2022 <ics3u.1.2022@mths.ca>
2022-09-26 13:18:23 +0000

    init

---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: Sep 2020
+# This program does basic math
+
+
+def main():
+    # this function does basic math
+
+    print(9 + 2)
+    print("7-3={}".format(7 - 3))
+    print("4÷2={}".format(4 / 2))
+    print("3+4×2={}".format(3 + 4 * 2))
+    print("5+2^3={}".format(5 + 2 ** 3))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
